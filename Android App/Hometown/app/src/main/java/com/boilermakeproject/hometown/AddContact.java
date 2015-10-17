4e1fd3a61a63b7c92da4866c43fe601916a54b44
Noah Rinehart <rinehart.noah@gmail.com>
2015-10-17 04:41:02 -0400

    too much too list

--- Android App/Hometown/app/src/main/java/com/boilermakeproject/hometown/AddContact.java
+++ Android App/Hometown/app/src/main/java/com/boilermakeproject/hometown/AddContact.java
@@ -1,13 +1,97 @@
 package com.boilermakeproject.hometown;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.sql.SQLException;
 
 public class AddContact extends AppCompatActivity {
+
+    private SQLController dbcon;
+    EditText nameText;
+    EditText homeText;
+    EditText numText;
+    EditText noteText;
+    Button saveButton;
+    Button deleteButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_add_contact);
+
+
+        nameText = (EditText) findViewById(R.id.contact_name);
+        homeText = (EditText)findViewById(R.id.hometown_add);
+        numText = (EditText)findViewById(R.id.contact_phone);
+        noteText = (EditText)findViewById(R.id.contact_note);
+
+        saveButton = (Button)findViewById(R.id.save_contact);
+        deleteButton = (Button)findViewById(R.id.delete_contact);
+
+
+        dbcon = new SQLController(getApplicationContext());
+        try{
+            dbcon.open();
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        if (this.getIntent().getExtras() != null) {
+            Intent intent = getIntent();
+            final String id = intent.getStringExtra("id");
+            final String name = intent.getStringExtra("name");
+            final String home = intent.getStringExtra("home");
+            final String lat = intent.getStringExtra("lat");
+            final String lon = intent.getStringExtra("long");
+            final String num = intent.getStringExtra("num");
+            final String note = intent.getStringExtra("note");
+            nameText.setText(name);
+            homeText.setText(home);
+            numText.setText(num);
+            noteText.setText(note);
+            saveButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dbcon.update(Long.parseLong(id), nameText.getText().toString(), "0", "0", homeText.getText().toString(), null, numText.getText().toString(), noteText.getText().toString());
+                    AddContact.this.finish();
+                }
+            });
+            deleteButton.setVisibility(View.VISIBLE);
+            deleteButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dbcon.delete(Long.parseLong(id));
+                    //insert dialog
+                    //Toast toast = new Toast.makeText(getApplicationContext(), "deleted " + name, Toast.LENGTH_SHORT).show();
+                    AddContact.this.finish();
+
+                }
+            });
+
+
+        }
+        if (this.getIntent().getExtras() == null){
+            nameText.setHint("Name");
+            homeText.setHint("Home");
+            numText.setHint("Phone Number");
+            noteText.setHint("Notes");
+        }
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dbcon.insert(nameText.getText().toString(), "0", "0", homeText.getText().toString(), null, numText.getText().toString(), noteText.getText().toString());
+                AddContact.this.finish();
+            }
+        });
+
+
+
     }
 }
--- Android App/Hometown/app/src/main/java/com/boilermakeproject/hometown/ContactFragment.java
+++ Android App/Hometown/app/src/main/java/com/boilermakeproject/hometown/ContactFragment.java
@@ -1,9 +1,12 @@
 package com.boilermakeproject.hometown;
 
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteException;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -12,11 +15,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
+import java.io.ByteArrayOutputStream;
 import java.sql.SQLException;
 
 /**
@@ -26,6 +33,29 @@ public class ContactFragment extends Fragment{
 
     private SQLController dbcon;
     private ListView listView;
+    FloatingActionButton fab;
+    SimpleCursorAdapter adapter;
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        Cursor cursor = dbcon.fetch();
+        String[] from = new String[] {
+                DBHelper._ID,
+                DBHelper.NAME,
+                DBHelper.HOMETOWN_NAME,
+                DBHelper.HOMETOWN_LAT,
+                DBHelper.HOMETOWN_LON,
+                DBHelper.IMAGE,
+                DBHelper.PHONE_NUM,
+                DBHelper.NOTE
+        };
+
+        int[] to = new int[]{R.id.id_holder, R.id.name, R.id.hometown, R.id.lat_holder, R.id.long_holder, R.id.image_holder, R.id.num_holder, R.id.note_holder};
+        adapter = new SimpleCursorAdapter(this.getContext(), R.layout.cantact_list_item, cursor, from, to);
+        adapter.notifyDataSetChanged();
+        listView.setAdapter(adapter);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -39,7 +69,16 @@ public class ContactFragment extends Fragment{
             e.printStackTrace();
         }
         listView = (ListView) view.findViewById(R.id.contact_list);
+        fab = (FloatingActionButton) view.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getContext(), AddContact.class);
+                startActivity(i);
+            }
+        });
 
+        //String name, String hometown_lat, String hometown_lon, String hometown_name, byte[] image, String phone_num, String note
 
 
         Cursor cursor = dbcon.fetch();
@@ -47,15 +86,15 @@ public class ContactFragment extends Fragment{
                 DBHelper._ID,
                 DBHelper.NAME,
                 DBHelper.HOMETOWN_NAME,
-                /*DBHelper.HOMETOWN_LAT,
+                DBHelper.HOMETOWN_LAT,
                 DBHelper.HOMETOWN_LON,
                 DBHelper.IMAGE,
                 DBHelper.PHONE_NUM,
-                DBHelper.NOTE*/
+                DBHelper.NOTE
         };
 
-        int[] to = new int[]{R.id.id, R.id.name, R.id.hometown};
-        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this.getContext(), R.layout.cantact_list_item, cursor, from, to);
+        int[] to = new int[]{R.id.id_holder, R.id.name, R.id.hometown, R.id.lat_holder, R.id.long_holder, R.id.image_holder, R.id.num_holder, R.id.note_holder};
+        adapter = new SimpleCursorAdapter(this.getContext(), R.layout.cantact_list_item, cursor, from, to);
         adapter.notifyDataSetChanged();
         listView.setAdapter(adapter);
 
@@ -63,9 +102,41 @@ public class ContactFragment extends Fragment{
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                TextView id_tv = (TextView) view.findViewById(R.id.id);
+                TextView id_tv = (TextView) view.findViewById(R.id.id_holder);
+                TextView name = (TextView) view.findViewById(R.id.name);
+                TextView home = (TextView) view.findViewById(R.id.hometown);
+                TextView lat = (TextView) view.findViewById(R.id.lat_holder);
+                TextView lon = (TextView) view.findViewById(R.id.long_holder);
+                ImageView image = (ImageView) view.findViewById(R.id.image_holder);
+                TextView num = (TextView) view.findViewById(R.id.num_holder);
+                TextView note = (TextView)view.findViewById(R.id.note_holder);
+
                 String id_string = id_tv.getText().toString();
-                Toast.makeText(getContext(), "Click ListItem Number " + position + "_id" + id_string, Toast.LENGTH_SHORT).show();
+                String name_string = name.getText().toString();
+                String home_string = home.getText().toString();
+                String lat_string = lat.getText().toString();
+                String lon_string = lon.getText().toString();
+
+//                Bitmap bm = image.getDrawingCache();
+//                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+//                bm.compress(Bitmap.CompressFormat.PNG, 100, stream);
+//                byte[] bytes = stream.toByteArray();
+
+                String num_string = num.getText().toString();
+                String note_string = note.getText().toString();
+
+                Intent modify_intent = new Intent(getContext(), AddContact.class);
+                modify_intent.putExtra("id", id_string);
+                modify_intent.putExtra("name", name_string);
+                modify_intent.putExtra("home", home_string);
+                modify_intent.putExtra("lat", lat_string);
+                modify_intent.putExtra("lon", lon_string);
+                modify_intent.putExtra("num", num_string);
+                modify_intent.putExtra("note", note_string);
+                startActivity(modify_intent);
+
+
+
             }
         });
 
